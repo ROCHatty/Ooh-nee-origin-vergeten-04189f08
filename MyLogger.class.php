67b2a9b4b480c578148dae10932a4df5be9ea227
Aaron Jonk <2119311@talnet.nl>
2020-05-26 13:22:00 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -7,7 +7,9 @@ class MyLogger
 	public function __contruct($origin = "")
 	{
 		if ($origin == "" || $origin == null)
+		{
 			die("[ERROR] Logger constructor needs origin!");
+		}
 		$this -> origin = $origin;
 	}
 	
